a8e8fe0a52355926fc4ace2a80cd7d2b1ba87d66
Inna Khomovska <innapariy@gmail.com>
2026-08-24 19:58:13 +0200

    feat: FA5 add routing and recipe details page layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { store } from "./store/store.ts";
 import App from "./App.tsx";
+import RecipeDetailsPage from "./features/recipes/RecipeDetailsPage.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/recipes/:id" element={<RecipeDetailsPage />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 );
